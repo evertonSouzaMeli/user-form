ed321b7f1c716e99999c3fe01e5aeb463a5a7777
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-04-24 13:24:13 -0300

    start of project

---
+++ src/main/java/br/com/fiap/bean/SetupBean.java
@@ -0,0 +1,47 @@
+package br.com.fiap.bean;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import br.com.fiap.dao.SetupDao;
+import br.com.fiap.model.Setup;
+
+@Named
+@RequestScoped
+public class SetupBean {
+
+	private Setup setup = new Setup();
+	private List<Setup> list;
+	
+	public SetupBean() {
+		list = this.list();
+	}
+	
+	public void save() {
+		System.out.println(this.setup);
+		new SetupDao().create(setup);
+	}
+	
+	public List<Setup> list(){
+		return new SetupDao().listAll();
+	}
+	
+	public List<Setup> getList() {
+		return list;
+	}
+
+	public void setList(List<Setup> list) {
+		this.list = list;
+	}
+
+	public Setup getSetup() {
+		return setup;
+	}
+
+	public void setSetup(Setup setup) {
+		this.setup = setup;
+	}
+
+}
